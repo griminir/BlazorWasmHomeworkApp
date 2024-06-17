a711461d73ae5df1d8b7807f5dfc9cdccea19fbb
griminir <viktor.elliot.king@gmail.com>
2024-06-17 11:52:12 +0200

    Add project files.

---
+++ BlazorWasmHomework/models/AddressModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWasmHomework.models
+{
+    public class AddressModel
+    {
+        [Required]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [StringLength(4, MinimumLength = 4)]
+        public string ZipCode { get; set; }
+    }
+}
